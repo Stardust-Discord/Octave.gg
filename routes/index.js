a5a55ac3803fda332d356e8dfbd4263e00d72126
Avarel <avarelpm@gmail.com>
2017-02-11 23:17:30 -0600

    Splash texts + fix stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index');
+let splash_texts = [
+    "Meme Extraordinaire",
+    "The Sarcastic yet Serious",
+    "The Cooliest There Is",
+    "Probably Tasty",
+    "Jet Fuel Can't Melt Steel Memes",
+    "Will Play Music for Food",
+    "Discord-senpai... I...",
+    "Finding the Rarest Pepe",
+    "Alternative Bots",
+    "90% Dank",
+    "It's High Noon"
+];
+
+/** GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', {
+        splash_text: splash_texts[Math.floor(Math.random() * splash_texts.length)]
+    });
 });
 
 module.exports = router;

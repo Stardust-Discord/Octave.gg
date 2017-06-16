17a2d148f0c29c2da77a49b0b311bfe06f60a2bd
Avarel <avarelpm@gmail.com>
2017-06-16 10:17:23 -0500

    Update Discord channel sidebar to match latest UI updates

--- public/js/discord-mockup.js
+++ public/js/discord-mockup.js
@@ -3,8 +3,7 @@
 let defaultId = 'default';
 
 function showGroup(id) {
-
-    if (typeof id == 'undefined') {
+    if (typeof id === 'undefined') {
         showGroup(defaultId);
         return;
     }
@@ -20,9 +19,24 @@ function showGroup(id) {
 
 $(document).ready(() => {
     showGroup(defaultId);
+
+    $('.trigger-group').each(function () {
+        if ($(this).attr('data') === defaultId) {
+            $(this).addClass('selected');
+        }
+    });
+
+    $(this).addClass('selected');
 });
 
 $('.trigger-group').click(function() {
     let id = $(this).attr('data');
+
+    $('.trigger-group').each(function () {
+        $(this).removeClass('selected');
+    });
+
+    $(this).addClass('selected');
+
     showGroup(id);
 });

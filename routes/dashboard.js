a51437aeec7bf2f4ba00acc4c35328e0b06858df
Avarel <avarelpm@gmail.com>
2017-06-23 18:35:46 -0500

    Updated looks

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -26,30 +26,32 @@ router.get('/login', (req, res) => {
 
 router.get('/callback', (req, res) => {
     discordAuth.code.getToken(req.originalUrl)
-        .then(function (user) {
+        .then(function (token) {
+            if (token.expired()) {
+                token.refresh().then(function (token) {
+                    req.session.discordToken = token.accessToken;
+                    request.get('https://discordapp.com/api/users/@me')
+                        .set('Authorization', `Bearer ${token.accessToken}`)
+                        .end(function(err, res2) {
+                            const json = JSON.parse(res2.text);
+                            return res.end(`refresh! you'ved logged in! ${json.username}`);
+                        })
+                });
+            } else {
+                req.session.discordToken = token.accessToken;
 
-            // user.refresh().then(function (updatedUser) {
-            //     console.log(updatedUser !== user); //=> true
-            //     console.log(updatedUser.accessToken)
-            // });
-
-            // We should store the token into a database.
-            //return res.send(user.accessToken)
-
-            req.session.discordToken = user.accessToken;
-
-            request.get('https://discordapp.com/api/users/@me')
-                .set('Authorization', `Bearer ${user.accessToken}`)
-                .end(function(err, res2) {
-                    const json = JSON.parse(res2.text);
-                    return res.end(`success! you'ved logged in! ${json.username}`);
-                })
+                request.get('https://discordapp.com/api/users/@me')
+                    .set('Authorization', `Bearer ${token.accessToken}`)
+                    .end(function(err, res2) {
+                        const json = JSON.parse(res2.text);
+                        return res.end(`success! you'ved logged in! ${json.username}`);
+                    })
+            }
         })
 });
 
-
 router.get('/showYourToken', (req, res) => {
-    res.end(req.session.discordToken)
+    res.end(req.session.discordToken);
 });
 
 module.exports = router;

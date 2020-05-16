1d07c42c103e1238c2de961fd7565c689f9a27b3
Melmsie <austin@melmsie.com>
2020-05-16 12:08:26 -0400

    push what I have bc I really need to work at the proper pc lmao

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('trust proxy', 1);
+app.set('view engine', 'html');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(bodyParser.json());
@@ -17,22 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', express.static(path.join(__dirname, 'public/index.html')));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  res.end('Not found!');
+// Handle 404
+app.use(function(req, res) {
+  res.status(404)
+  res.redirect('/404.html')
 });
 
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Handle 500
+app.use(function(error, req, res, next) {
+  res.send('500: Internal Server Error' + error, 500);
+  console.log
 });
 
 module.exports = app;
